0dabc2e6313e595b5580138ea6e931fbe7511853
leonid0801 <93707426@qq.com>
2018-07-10 11:54:50 +0800

    add parameters

--- Nginx/config/config.php
+++ Nginx/config/config.php
@@ -15,3 +15,4 @@ define ( "DOMAIN_NAME", "https://bjwob.top") ;
 define ( "FULL_ITEM_DIR", APP_PATH.'/'.ITEM_DIR) ;
 define ( "FULL_ITEM_DIR_DOMAIN", DOMAIN_NAME.'/'.ITEM_DIR) ;
 define ( "SUPER_ADMIN", '10177') ;
+define ( "DEFAULT_VIEW_COUNT", '200') ;
