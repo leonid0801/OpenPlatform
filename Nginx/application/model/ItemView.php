abf5158f3cec5a2bf4215a05becc8adb44876ad3
leonid0801 <93707426@qq.com>
2018-07-10 11:57:20 +0800

    added

---
+++ Nginx/application/model/ItemView.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ *
+ * @ 
+ *
+ */
+class ItemView extends DBModel {
+
+	/**
+	 *
+	 * @param unknown $db_config_name
+	 *
+	 * @param unknown $table
+	 *
+	 * @throws Exception
+	 *
+	 */
+	public function __construct($table) {
+		parent::__construct ($table,parent::DEFAULT_DB_CONF);
+	}
+	
+	public function  addClient($wxopenid_info){
+		//$wxopenid_info["Finsert_time"] = date( "Y-m-d H:i:s",time() )  ;	
+		return $this->insert($wxopenid_info);
+	}
+	
+	public function getUserByOpenid($wxopenid){
+    	return $this->select(parent::ALL_FIELD, "Fopenid='{$wxopenid}'");
+    }
+
+	public function insertInfo($params){
+		return $this->insert($params);
+	}
+
+    public function increaseView($itemId){
+        //update cmf_posts set post_hits=post_hits+'1' where id=41
+        $sql="update t_view set f_count=f_count+'1' where f_itemid='{$itemId}'";
+        return $this->executeSql($sql);
+    }
+
+    public function insertWithIdRes($params){
+        return $this->insertBackLastID($params);
+    }
+	//获取订单
+	public function getItemList($where,$start=null,$offset=null){
+		return $this->select('*',$where,null,' f_created DESC limit 3',$start,$offset);
+	}
+
+    public function getMoreItemList($where,$start,$offset){
+        return $this->select('*',$where,null,' f_created DESC ',$start,$offset);
+    }
+
+    public function getItemByFid($item_id){
+        return $this->select(parent::ALL_FIELD, "f_itemid='{$item_id}'");
+    }
+
+    public function getCountByFid($item_id){
+        return $this->select(Array("f_count"), "f_itemid='{$item_id}'");
+    }
+
+    public function getUidByItemid($item_id){
+        return $this->select(Array("f_uid"), "f_itemid='{$item_id}'");
+    }
+
+    public function del($where){
+        return $this->delete($where);
+    }
+
+	//设置为已读
+	public function setRead($info,$where){
+		return $this->update($info,$where);
+	}
+	//获取总记录数
+	public function getTotalNum($where){
+		return $this->count($where);
+	}
+	//执行sql
+	public function executeSql($sql){
+
+		return $this->execute($sql, false);
+	}
+}
+
+?>
